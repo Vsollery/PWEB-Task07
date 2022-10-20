7ea3b707d8008c76c77df24318fd6fe97d625a08
Venia <vsollery@gmail.com>
2022-10-20 21:44:44 +0700

    initial

---
+++ script/d3_2.js
@@ -0,0 +1,60 @@
+// set the dimensions and margins of the graph
+var margin = {top: 10, right: 30, bottom: 40, left: 100},
+    width = 460 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg1 = d3.select("#lollipop")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+// Parse the Data
+//d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum_header.csv", function(data) {
+d3.csv("https://raw.githubusercontent.com/Vsollery/Fruits/main/Fruits.csv", function(data) {
+  // Add X axis
+  var x = d3.scaleLinear()
+    .domain([0, 80])
+    .range([ 0, width]);
+  svg1.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+      .attr("transform", "translate(-10,0)rotate(-45)")
+      .style("text-anchor", "end");
+
+// Y axis
+var y = d3.scaleBand()
+  .range([ 0, height ])
+  .domain(data.map(function(d) { return d.Fruits; }))
+  .padding(1);
+svg1.append("g")
+  .call(d3.axisLeft(y))
+
+
+// Lines
+svg1.selectAll("myline")
+  .data(data)
+  .enter()
+  .append("line")
+    .attr("x1", function(d) { return x(d.Amounts); })
+    .attr("x2", x(0))
+    .attr("y1", function(d) { return y(d.Fruits); })
+    .attr("y2", function(d) { return y(d.Fruits); })
+    .attr("stroke", "blue")
+
+// Circles
+svg1.selectAll("mycircle")
+  .data(data)
+  .enter()
+  .append("circle")
+    .attr("cx", function(d) { return x(d.Amounts); })
+    .attr("cy", function(d) { return y(d.Fruits); })
+    .attr("r", "4")
+    .style("fill", "#AA4A44")
+    .attr("stroke", "#AA4A44")
+})
+
---
+++ script/google.js
@@ -0,0 +1,103 @@
+google.charts.load('current', {
+  packages: ['corechart'],
+});
+
+google.charts.setOnLoadCallback(drawChart);
+
+const pieChart = {
+    chart: null,
+    data: [
+      ["Fruits", "Amounts"],
+      ["Oranges", 63.31],
+      ["Apples", 47.9],
+      ["Bananas", 25.61],
+      ["Melons", 25.56],
+      ["Grapes", 18.59],
+      ["Pineapples", 12.75],
+      ["Strawberry", 9.21],
+      ["Peaches", 8.79],
+      ["Grapefruit", 7.53],
+      ["Lemons", 6.72],
+    ],
+    element: '#pie-chart',
+    options: {
+      title: 'The most consumed fruits in the USA',
+      width: 800,
+      height: 500
+    }
+  };
+  
+  const barChart = {
+    chart: null,
+    data: [
+      ["Fruits", "Amounts"],
+      ["Oranges", 63.31],
+      ["Apples", 47.9],
+      ["Bananas", 25.61],
+      ["Melons", 25.56],
+      ["Grapes", 18.59],
+      ["Pineapples", 12.75],
+      ["Strawberry", 9.21],
+      ["Peaches", 8.79],
+      ["Grapefruit", 7.53],
+      ["Lemons", 6.72],
+    ],
+    element: '#bar-chart',
+    options:  {
+      title: 'The most consumed fruits in the USA',
+      width: 800,
+      height: 500
+    }
+  };
+  
+  const lineChart = {
+    chart: null,
+    data: [
+      ["Fruits", "Amounts"],
+      ["Oranges", 63.31],
+      ["Apples", 47.9],
+      ["Bananas", 25.61],
+      ["Melons", 25.56],
+      ["Grapes", 18.59],
+      ["Pineapples", 12.75],
+      ["Strawberry", 9.21],
+      ["Peaches", 8.79],
+      ["Grapefruit", 7.53],
+      ["Lemons", 6.72],
+    ],
+    element: '#line-chart',
+    options: {
+      title: 'The most consumed fruits in the USA',
+      width: 800,
+      height: 500
+    }
+  };
+
+  function drawChart (){
+    pieChart.chart = new google.visualization.PieChart(
+      document.querySelector(pieChart.element)
+    );
+    pieChart.chart.draw(
+      google.visualization.arrayToDataTable(pieChart.data),
+      pieChart.options
+    );
+    
+    barChart.chart = new google.visualization.BarChart(
+      document.querySelector(barChart.element)
+    );
+    barChart.chart.draw(
+      google.visualization.arrayToDataTable(barChart.data),
+      barChart.options
+    );
+    
+    lineChart.chart = new google.visualization.LineChart(
+      document.querySelector(lineChart.element)
+    );
+    lineChart.chart.draw(
+      google.visualization.arrayToDataTable(lineChart.data),
+      lineChart.options
+    );
+  };
+  
+
+  
